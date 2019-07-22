dfcb0d837e15b4e155ccdb289925849daeb79d1a
rahulchahar <38423112+rahulchahar@users.noreply.github.com>
2019-07-22 23:10:43 +0530

    Add files via upload
    initial commit

---
+++ client/components/CoinChart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Example from './LineChart';
+import StreamingDemo from './DynChart';
+
+class CoinChart extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			data:null
+		}
+	}
+	componentDidMount(){
+	let path= ((window.location.pathname).split("/"))[2];
+	this.setState({
+		data: path
+	});
+	}
+	render(){return(<div className="chart"><StreamingDemo coin={this.state.data}/></div>)}}
+
+export default CoinChart;
